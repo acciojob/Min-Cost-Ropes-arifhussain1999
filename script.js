fd2bd30f1740dcd1ccfc371492b1a4ebc3f133e9
Arif Hussain <arifh9821@gmail.com>
2024-01-14 23:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let totalCost=0;
+  while (arr.length>1) {
+  	arr.sort((a,b)=>a-b)
+	  let e1=arr[0];
+	  let e2=arr[1];
+	  arr.splice(0,2);
+	  arr.push(e1+e2);
+	  totalCost+=e1+e2;
+  }
+	return totalCost;
 }
 
 module.exports=mincost;
